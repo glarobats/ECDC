478fdf63d4d4cc14d8649f4262d8f1e2168fafe3
Ioannis Kondilis <glarobats@gmail.com>
2024-03-17 21:05:27 +0200

    first mapreduce completed

--- src/main/java/ecdc/ecdc.java
+++ src/main/java/ecdc/ecdc.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -24,8 +25,8 @@ public class ecdc {
         Job job = new Job(conf, "ncdc");
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setMapperClass(Map.class);
-      //  job.setReducerClass(Reduce.class);
+        job.setMapperClass(FirstMap.class);
+        job.setReducerClass(FirstReducer.class);
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
@@ -34,49 +35,51 @@ public class ecdc {
     }
 
 
-    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text data = new Text();
+    public static class FirstMap extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private HashMap<String, HashMap<String, IntWritable>> dataMap = new HashMap<>();
 
-
-        // Αναλύει το κείμενο και δημιουργεί νέο πίνακα
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            // Split the CSV line by comma
-            String[] line = value.toString().split(",");
-
-            if (line.length > 0) {
-                List<String> dataList = new ArrayList<>();
-
-                // Iterate over the elements of the CSV line
-                for (int i = 0; i < line.length; i++) {
-                    // Skip the first element
-                    if (i == 0) {
-                        continue;
-                    }
-                    dataList.add(line[i].trim()); // Add each element to the dataList
-                }
-
-                // Print the dataList
-                System.out.println(dataList);
+            // Παράβλεψη της πρώτης γραμμής
+            if (key.get() == 0) {
+                return;
             }
-        }
+            // Χωρίζει το κείμενο σε γραμμές
+            String[] line = value.toString().split(",");
+            // Λήψη συγκεκριμένων δεδομένων από τη γραμμή
+            String month = line[2];
+            int cases = Integer.parseInt(line[4]);
+            String country = line[6];
+
+            // Δημιουργία νέου χάρτη αν δεν υπάρχει ήδη για τη χώρα
+            if (!dataMap.containsKey(country))
+                dataMap.put(country, new HashMap<>());
+
+            // Προσθήκη του αριθμού κρουσμάτων για τον συγκεκριμένο μήνα
+            dataMap.get(country).put(month, new IntWritable(cases));
+
+            // Εκπομπή του κλειδιού-τιμής (country, cases)
+            context.write(new Text(country + " " + month), new IntWritable(cases));        }
     }
 
 
-    /*
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+
+    public static class FirstReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-
             int sum = 0;
+            int count = 0;
 
-            // Αθροίζει τα n-grams
-            for (IntWritable val : values)
-                sum += val.get();
+            // Calculate the sum of cases and count the number of months
+            for (IntWritable value : values) {
+                sum += value.get();
+                count++;
+            }
 
-            // Επιστρέφει το n-gram και το άθροισμα
-            context.write(key, new IntWritable(sum));
+            // Calculate the average
+            double average = (double) sum / count;
+
+            // Emit the country and its average number of cases per month
+            context.write(key, new DoubleWritable(average));
         }
     }
-
-         */
 }
